1b2faece234dec48be5e6299bd9332c352964e4e
ZINEB6677 <zinebalmahir@gmail.com>
2026-02-21 06:47:56 +0000

    up

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -3,15 +3,15 @@ import { useRouter } from "next/router";
 import PropertyDetail from "@/components/property/PropertyDetail";
 
 export default function PropertyPage() {
-    const router = useRouter();
-    const { id } = router.query;
-    const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
+  const router = useRouter();
+  const { id } = router.query;
+  const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
 
-    if (!property) return <p>Property not found</p>;
+  if (!property) return <p>Property not found</p>;
 
-    return (
-        <div>
-            <PropertyDetail property={property} />
-        </div>
-    );
+  return (
+    <div>
+      <PropertyDetail property={property} />
+    </div>
+  );
 }
